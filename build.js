0f7340d78ab1b7c3e39c407acf2f428c9c3ecffc
ylxdzsw <ylxdzsw@gmail.com>
2018-08-08 18:02:35 +0800

    another round of reforming

--- build.js
+++ build.js
@@ -3,10 +3,11 @@ Copyright (c) 2018: Zhang Shiwei (ylxdzsw@gmail.com)
 
 This script build the blog by:
 1. get a list of posts by find main.* in `_posts` folder recursively
-2. delete compiled posts that do not appear in the above list
-3. compare the hashs of posts with those in `info.json` and set update timestamps
-4. recompile posts that hashes do not match, then update `info.json`
-5. generate the index page
+2. compare the hashs of posts with those in `info.json` and set update timestamps
+3. recompile posts that hashes do not match, then update `info.json`
+4. handle links in `links.txt`
+5. delete compiled posts that do not appear in the source
+6. generate the index page
 */
 
 "don't use strict"
@@ -19,10 +20,13 @@ const path = require('path')
 const crypto = require('crypto')
 
 class Post {
-    constructor(path) {
-        this.path = path
+    constructor(p) {
+        this.path = p
         this.hash = this.getHash()
-        this.name = this.getName()
+        this.link = this.getLink()
+        if (!this.link.startsWith('_')) {
+            this.name = path.parse(this.link).name
+        }
     }
 
     getHash() {
@@ -49,13 +53,13 @@ class Post {
 }
 
 class JadePost extends Post {
-    getName() {
+    getLink() {
         return path.basename(this.path) + '.html'
     }
 
     compile() {
         const target = path.join(this.path, "main.jade")
-        const result = path.join(__dirname, this.getName())
+        const result = path.join(__dirname, this.link)
         return new Promise((resolve, reject) => {
             cp.exec(`nattoppet ${target} > ${result}`, (err) => err ? reject(err) : resolve())
         })
@@ -63,13 +67,13 @@ class JadePost extends Post {
 }
 
 class HTMLPost extends Post {
-    getName() {
+    getLink() {
         return path.basename(this.path) + '.html'
     }
 
     compile() {
         const src = path.join(this.path, "main.html")
-        const dst = path.join(__dirname, this.getName())
+        const dst = path.join(__dirname, this.link)
         return new Promise((resolve, reject) => {
             fs.copyFile(src, dst, (err) => err ? reject(err) : resolve())
         })
@@ -77,7 +81,7 @@ class HTMLPost extends Post {
 }
 
 class TeXPost extends Post {
-    getName() {
+    getLink() {
         return path.basename(this.path) + '.pdf'
     }
 
@@ -98,7 +102,7 @@ class TeXPost extends Post {
                 if (err) return reject(err)
 
                 const src = path.join(this.path, "main.pdf")
-                const dst = path.join(__dirname, this.getName())
+                const dst = path.join(__dirname, this.link)
                 fs.copyFile(src, dst, (err) => err ? reject(err) : resolve())
             })
         }).then(v => new Promise((resolve, reject) => clean_up().then(x=>resolve(v))),
@@ -107,13 +111,13 @@ class TeXPost extends Post {
 }
 
 class PDFPost extends Post {
-    getName() {
+    getLink() {
         return path.basename(this.path) + '.pdf'
     }
 
     compile() {
         const src = path.join(this.path, "main.pdf")
-        const dst = path.join(__dirname, this.getName())
+        const dst = path.join(__dirname, this.link)
         return new Promise((resolve, reject) => {
             fs.copyFile(src, dst, (err) => err ? reject(err) : resolve())
         })
@@ -145,15 +149,7 @@ const walk = dir => {
 
 walk(path.join(__dirname, "_posts"))
 
-// step 2. delete compiled posts that do not appear in the above list
-
-for (const p of fs.readdirSync(__dirname)
-                  .filter(x=>x.endsWith('.html') || x.endsWith('.pdf'))
-                  .filter(x=>!x.startsWith('google') && !x.startsWith('index'))) {
-    posts.some(x => x.name == p) || fs.unlink(path.join(__dirname, p), ()=>0)
-}
-
-// step 3. compare the hashs of posts with those in `info.json` and set update timestamps
+// step 2. compare the hashs of posts with those in `info.json` and set update timestamps
 
 const hashdict = Object.create(null)
 
@@ -165,7 +161,7 @@ for (const p of JSON.parse(fs.readFileSync(path.join(__dirname, 'info.json'), { 
     if (x) x.timestamp = p.timestamp
 }
 
-// step 4. recompile posts that hashes do not match, then update `info.json`
+// step 3. recompile posts that hashes do not match, then update `info.json`
 
 const tasks = []
 let infostr = '[\n'
@@ -180,7 +176,33 @@ for (const p of posts.sort((a, b) => a.hash > b.hash ? 1 : -1)) {
 
 fs.writeFileSync(path.join(__dirname, 'info.json'), infostr.slice(0, -2) + '\n]')
 
-// step 5. generate the index page
+// step 4. handle links in `links.txt`
+
+const links = fs.readFileSync(path.join(__dirname, 'links.txt')).toString().split('\n')
+
+for (const link of links) {
+    const [src, dest, time] = link.split(' ')
+    if (time) {
+        posts.push({
+            name: src,
+            link: dest,
+            timestamp: parseInt(time)
+        })
+    } else {
+        fs.writeFileSync(path.join(__dirname, src), `<!DOCTYPE HTML><meta charset="UTF-8"><meta http-equiv="refresh" content="0; url=${dest}"><title>Redirection</title>This page has been moved to <a href="${dest}">${dest}</a>`)
+        posts.push({ link: src })
+    }
+}
+
+// step 5. delete compiled posts that do not appear in the source
+
+for (const p of fs.readdirSync(__dirname)
+                  .filter(x=>x.endsWith('.html') || x.endsWith('.pdf'))
+                  .filter(x=>!x.startsWith('google') && !x.startsWith('index'))) {
+    posts.some(x => x.link == p) || fs.unlink(path.join(__dirname, p), ()=>0)
+}
+
+// step 6. generate the index page
 
 const stale_color = time => {
     const diff = now - time
@@ -191,11 +213,11 @@ const stale_color = time => {
            diff < 86400000000 ? 'tomato' : 'red' // 1000 days
 }
 
-const index_head = `<!DOCTYPE html><html><head><meta charset=utf8><meta name=viewport content="width=device-width"><title>ylxdzsw's blog</title><style>li{padding-left:0.8em;list-style-type:none}@media(max-width:415px){.detail{display:none}}</style></head><body><h1 style="display:inline;margin:0.2em 0.5em 0.2em 0.2em">ylxdzsw's blog</h1><a target="_blank" href="_about.html">About me</a><hr>\n`
-const index_foot = `<hr><p>Copyright © 2015-2018: root@ylxdzsw.com</p></body></html>`
-const index_body = posts.filter(x => !x.name.startsWith('_'))
+const index_head = `<!DOCTYPE HTML><meta charset="UTF-8"><meta name=viewport content="width=device-width"><title>ylxdzsw's blog</title><style>li{padding-left:0.8em;list-style-type:none}@media(max-width:415px){.detail{display:none}}</style><h1 style="display:inline;margin:0.2em 0.5em 0.2em 0.2em">ylxdzsw's blog</h1><a target="_blank" href="_about.html">About me</a><hr>\n`
+const index_foot = `<hr><p>Copyright © 2015-2018: root@ylxdzsw.com</p>`
+const index_body = posts.filter(x => x.name)
                         .sort((a, b) => a.name < b.name ? 1 : -1)
-                        .map(x => `<li style="border-left:solid ${stale_color(x.timestamp)}"><a target="_blank" href="${x.name}">${path.basename(x.path)}</a> <span class="detail" style="color:gray;font-size:0.85em">(last update at ${new Date(x.timestamp).toLocaleString()})</span></li>\n`)
+                        .map(x => `<li style="border-left:solid ${stale_color(x.timestamp)}"><a target="_blank" href="${x.link}">${x.name}</a> <span class="detail" style="color:gray;font-size:0.85em">(last update at ${new Date(x.timestamp).toLocaleString()})</span></li>\n`)
                         .join('')
 
 fs.writeFileSync(path.join(__dirname, 'index.html'), index_head + index_body + index_foot)
