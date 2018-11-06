3fc88850900e4facb6d82512c94f569531d951b9
ylxdzsw <ylxdzsw@gmail.com>
2018-11-06 22:07:02 +0800

    update

--- build.js
+++ build.js
@@ -10,6 +10,7 @@ This script build the blog by:
 4. handle links in `links.txt`
 5. delete compiled posts that do not appear in the source
 6. generate the index page
+7. generate the RSS feed
 */
 
 "don't use strict"
@@ -218,7 +219,9 @@ const stale_color = time => {
            diff < 86400000000 ? 'tomato' : 'red' // 1000 days
 }
 
-const index_head = `<!DOCTYPE HTML><meta charset="UTF-8"><meta name=viewport content="width=device-width"><title>ylxdzsw's blog</title><style>li{padding-left:0.8em;list-style-type:none}@media(max-width:415px){.detail{display:none}}</style><h1 style="display:inline;margin:0.2em 0.5em 0.2em 0.2em">ylxdzsw's blog</h1><a target="_blank" href="_about.html">About me</a><hr>\n`
+const RSS_icon = `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="1rem" height="1rem" id="RSSicon" viewBox="0 0 256 256"><defs><linearGradient x1="0.085" y1="0.085" x2="0.915" y2="0.915" id="RSSg"><stop  offset="0.0" stop-color="#E3702D"/><stop  offset="0.1071" stop-color="#EA7D31"/><stop  offset="0.3503" stop-color="#F69537"/><stop  offset="0.5" stop-color="#FB9E3A"/><stop  offset="0.7016" stop-color="#EA7C31"/><stop  offset="0.8866" stop-color="#DE642B"/><stop  offset="1.0" stop-color="#D95B29"/></linearGradient></defs><rect width="256" height="256" rx="55" ry="55" x="0"  y="0"  fill="#CC5D15"/><rect width="246" height="246" rx="50" ry="50" x="5"  y="5"  fill="#F49C52"/><rect width="236" height="236" rx="47" ry="47" x="10" y="10" fill="url(#RSSg)"/><circle cx="68" cy="189" r="24" fill="#FFF"/><path d="M160 213h-34a82 82 0 0 0 -82 -82v-34a116 116 0 0 1 116 116z" fill="#FFF"/><path d="M184 213A140 140 0 0 0 44 73 V 38a175 175 0 0 1 175 175z" fill="#FFF"/></svg>`
+
+const index_head = `<!DOCTYPE HTML><meta charset="UTF-8"><meta name=viewport content="width=device-width"><title>ylxdzsw's blog</title><style>li{padding-left:0.8em;list-style-type:none}@media(max-width:415px){.detail{display:none}}</style><h1 style="display:inline;margin:0.2em 0.5em 0.2em 0.2em">ylxdzsw's blog</h1><a target="_blank" href="feed.xml" style="margin-right:1em">${RSS_icon}</a><a target="_blank" href="_about.html">About me</a><hr>\n`
 const index_foot = `<hr><p>Copyright © 2015-2018: root@ylxdzsw.com</p>`
 const index_body = posts.filter(x => x.name)
                         .sort((a, b) => a.name < b.name ? 1 : -1)
@@ -226,6 +229,18 @@ const index_body = posts.filter(x => x.name)
                         .join('')
 
 fs.writeFileSync(path.join(__dirname, 'index.html'), index_head + index_body + index_foot)
+
+// Step 7. generate the RSS feed
+
+const feed_head = `<?xml version="1.0" encoding="utf-8"?><rss version="2.0"><channel><title>ylxdzsw's blog</title><link>https://blog.ylxdzsw.com/</link><description>Works, thoughts, life, and balderdash.</description><lastBuildDate>${new Date(now).toUTCString()}</lastBuildDate>`
+const feed_foot = `</channel></rss>`
+const feed_body = posts.filter(x => x.name)
+                       .sort((a, b) => a.name < b.name ? 1 : -1)
+                       .slice(0, 15)
+                       .map(x => `<item><title>${x.name}</title><link>https://blog.ylxdzsw.com/${x.link}</link><guid>${x.hash}</guid><pubDate>${new Date(x.timestamp).toUTCString()}</pubDate></item>`)
+                       .join('')
+
+fs.writeFileSync(path.join(__dirname, 'feed.xml'), feed_head + feed_body + feed_foot)
 
 // wait and done
 
