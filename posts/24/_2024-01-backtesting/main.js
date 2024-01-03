f534f2d2b9da854b51953efb0a0e9fd0b35fa746
Shiwei Zhang <ylxdzsw@gmail.com>
2024-01-03 18:34:37 +0800

    .

--- posts/24/_2024-01-backtesting/main.js
+++ posts/24/_2024-01-backtesting/main.js
@@ -23,16 +23,20 @@ const app = {
 
     log(msg) {
         app.emit("log", msg)
-        console.log(msg)
     },
 
-    async backtest(strategy, trace=app.active_dataset, log=true) {
+    async backtest(strategy, opts) {
         const strategy_fun = new Function("with (this) { " + strategy + " }")
-        const history = []
-        for (let i = 0; i < trace.length; i++) {
-            const env = new BacktestEnvironment(trace, history, i)
+        const ctx = {
+            history: [],
+            candles: app.active_dataset,
+            log: true,
+            ...opts
+        }
+        for (let i = 0; i < ctx.candles.length; i++) {
+            const env = new BacktestEnvironment(ctx, i)
             strategy_fun.call(env)
-            if (i == trace.length - 1)
+            if (i == ctx.candles.length - 1)
                 if (env.holding > 0) // force selling at the end
                     env.sell()
                 else // and prevent buying
@@ -40,12 +44,12 @@ const app = {
 
             switch (env.action) {
                 case "buy":
-                    log && env.log(`买入${(env.cash / env.price).toFixed(2)}份标的，价格${env.price.toFixed(2)}`)
+                    ctx.log && env.log(`买入${(env.cash / env.price).toFixed(2)}份标的，价格${env.price.toFixed(2)}`)
                     env.cash_after_action = 0
                     env.holding_after_action = env.cash / env.price
                     break
                 case "sell":
-                    log && env.log(`卖出${env.holding.toFixed(2)}份标的，价格${env.price.toFixed(2)}，收益${(100*env.profit).toFixed(2)}%`)
+                    ctx.log && env.log(`卖出${env.holding.toFixed(2)}份标的，价格${env.price.toFixed(2)}，收益${(100*env.profit).toFixed(2)}%`)
                     env.cash_after_action = env.holding * env.price
                     env.holding_after_action = 0
                     break
@@ -54,45 +58,58 @@ const app = {
                     env.holding_after_action = env.holding
             }
 
-            if (env.logged) await new Promise(resolve => setTimeout(resolve, 0))
+            i % 32 == 1 && await new Promise(resolve => setTimeout(resolve, 0))
 
-            history.push(env)
+            ctx.history.push(env)
         }
-        return history[history.length - 1].cash_after_action
+        return ctx.history
+    },
+
+    analyze_backtest_result(history) {
+        const total_days = history.length - 1
+        const total_profit = history[history.length - 1].cash_after_action / 10000 - 1
+        const holding_days = history.filter(x => x.action == 'sell').map(x => x.holding_days).reduce((a, b) => a + b, 0)
+        const max_profit = history.filter(x => x.action == 'sell').map(x => x.profit).reduce((a, b) => Math.max(a, b), 0)
+        const min_profit = history.filter(x => x.action == 'sell').map(x => x.profit).reduce((a, b) => Math.min(a, b), 0)
+        return `\
+=== 回测结果 ===
+总日数：${total_days}
+总收益：${(total_profit * 100).toFixed(2)}%，年化：${(100 * Math.pow(1 + total_profit, 365 / total_days) - 100).toFixed(2)}%
+持仓日数：${holding_days}，占总日数：${(100 * holding_days / total_days).toFixed(2)}%
+最大单次持仓收益：${(100 * max_profit).toFixed(2)}%，亏损：${(100 * min_profit).toFixed(2)}%
+`
     }
 }
 
 
 class BacktestEnvironment {
-    constructor(candles, history, day) {
-        this.candles = candles
-        this.history = history
+    constructor(ctx, day) {
+        this.ctx = ctx
         this.day = day
 
         this.action = null // to be filled by strategy
-        this.logged = false // We will await if any logging message is printed.
         this.cash_after_action = null // to be filled after calling strategy
         this.holding_after_action = null // to be filled after calling strategy
     }
 
     get open() {
-        return this.candles[this.day].open
+        return this.ctx.candles[this.day].open
     }
 
     get close() {
-        return this.candles[this.day].close
+        return this.ctx.candles[this.day].close
     }
 
     get high() {
-        return this.candles[this.day].high
+        return this.ctx.candles[this.day].high
     }
 
     get low() {
-        return this.candles[this.day].low
+        return this.ctx.candles[this.day].low
     }
 
     get volume() {
-        return this.candles[this.day].volume
+        return this.ctx.candles[this.day].volume
     }
 
     get price() {
@@ -109,11 +126,26 @@ class BacktestEnvironment {
         return this.t(-1).holding_after_action
     }
 
-    get profit() {
+    get holding_days() {
         if (!this.holding) return 0
         for (let i = -1; i >= -this.day; i--)
-            if (this.t(i).action == "buy")
-                return this.holding * this.price / this.t(i).cash - 1
+            if (this.t(i).action == 'buy')
+                return -i
+    }
+
+    get holding_price() {
+        if (!this.holding) return 0
+        return this.t(-this.holding_days).price
+    }
+
+    get profit() {
+        if (!this.holding) return 0
+        return this.holding * this.price / this.t(-this.holding_days).cash - 1
+    }
+
+    get profit_annualized() {
+        if (!this.holding) return 0
+        return Math.pow(1 + this.profit, 365 / this.holding_days) - 1
     }
 
     buy() {
@@ -127,13 +159,12 @@ class BacktestEnvironment {
     t(i) {
         if (i > 0) throw new Error("t(i) i must be negative")
         if (i == 0) return this
-        if (this.day + i < 0) return this.history[0] ?? this
-        return this.history[this.day + i]
+        if (this.day + i < 0) return this.ctx.history[0] ?? this
+        return this.ctx.history[this.day + i]
     }
 
     log(msg) {
-        app.log(`${new Date(this.candles[this.day].timestamp).toLocaleDateString()}：${msg}`)
-        this.logged = true
+        app.log(`${new Date(this.ctx.candles[this.day].timestamp).toLocaleDateString()}：${msg}`)
     }
 
     MA(n) {
@@ -146,7 +177,9 @@ class BacktestEnvironment {
 
     EMA(n) {
         if (this.day == 0) return this.price
-        return this.price * 2 / (n + 1) + this.t(-1).EMA(n) * (n - 1) / (n + 1)
+        const key = `EMA_${n}`
+        this[key] ??= this.price * 2 / (n + 1) + this.t(-1).EMA(n) * (n - 1) / (n + 1)
+        return this[key]
     }
 
     DIF(fast=12, slow=26) {
@@ -155,7 +188,9 @@ class BacktestEnvironment {
 
     DEA(fast=12, slow=26, n=9) {
         if (this.day == 0) return this.DIF(fast, slow)
-        return this.DIF(fast, slow) * 2 / (n + 1) + this.t(-1).DEA(fast, slow, n) * (n - 1) / (n + 1)
+        const key = `DEA_${fast}_${slow}_${n}`
+        this[key] ??= this.DIF(fast, slow) * 2 / (n + 1) + this.t(-1).DEA(fast, slow, n) * (n - 1) / (n + 1)
+        return this[key]
     }
 
     MACD(fast=12, slow=26, n=9) {
